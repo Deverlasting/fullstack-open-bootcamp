2337b1d310c872f4a1aafa1a9e312be8094f3bdf
Ernesto Díaz <srquitanieves@gmail.com>
2024-04-27 17:24:45 +0200

    Delete part2 directory

--- part2/agenda/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-export const Filter = ({ filterText, handleChangeFilterText }) => {
-  return (
-    <div>
-      Filter: <input value={filterText} onChange={handleChangeFilterText} />
-    </div>
-  )
-}
-
-export default Filter
