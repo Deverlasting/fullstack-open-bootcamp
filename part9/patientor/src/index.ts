784eb1867e0c8a6f0dabc11ed5b5a4f37a7d8550
HeruhD <srquitanieves@gmail.com>
2024-06-20 18:46:02 +0200

    20/06/24 part9 ex9.10 WIP

---
+++ part9/patientor/src/index.ts
@@ -0,0 +1,23 @@
+// import { Diagnose } from './types';
+import express from 'express';
+import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
+// import diagnoseData from '../data/diagnoses'
+
+const app = express();
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3003;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use('/api/diagnoses', diagnoseRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
